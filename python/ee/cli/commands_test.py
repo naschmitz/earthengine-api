f91bde024c7e9f86264c2eae53052ad08ba0f395
Nathaniel Schmitz <schmitznathaniel@gmail.com>
2024-08-02 16:10:00 +0000

    Make a test that fails.

---
+++ python/ee/cli/commands_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Test for the commands module."""
+
+import unittest
+
+
+class CommandsTest(unittest.TestCase):
+
+    def test_command(self):
+        self.assertFalse(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
