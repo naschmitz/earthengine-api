a539b78c8d2d6e841f518fee73c54f435b3fb19b
Nathaniel Schmitz <schmitznathaniel@gmail.com>
2024-08-02 16:48:54 +0000

    Add tests that might fail on Windows.

--- python/ee/cli/commands_test.py
+++ python/ee/cli/commands_test.py
@@ -3,11 +3,18 @@
 
 import unittest
 
+from ee.cli import commands
+
 
 class CommandsTest(unittest.TestCase):
 
     def test_command(self):
-        self.assertFalse(True)
+        self.assertEqual(
+            commands._cloud_timestamp_for_timestamp_ms(-1000), "1969-12-31T23:59:59Z"
+        )
+        self.assertEqual(
+            commands._cloud_timestamp_for_timestamp_ms(1000), "1970-01-01T00:00:01Z"
+        )
 
 
 if __name__ == "__main__":
